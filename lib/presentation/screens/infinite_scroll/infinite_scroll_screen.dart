9cef27439e7fa7cf40d6e378fc6f9cec5b1dff3f
alejo Gonzalez <alejo.g31@hotmail.com>
2023-07-15 19:24:56 -0500

    Se agregan las nuevas pantallas con su archivo de barril

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InifinitiScrollScreen extends StatelessWidget {
+  static const name = 'infinite_scroll_screen';
+
+  const InifinitiScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/progres/progres_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProgresScreen extends StatelessWidget {
+  static const name = 'progres_screen';
+
+  const ProgresScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
