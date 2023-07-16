03b8a7f3c1e4ae0532b1ccb0e743ff88422fea8a
alejo Gonzalez <alejo.g31@hotmail.com>
2023-07-15 19:02:12 -0500

    Agregado cards con imagen

--- lib/presentation/cards/cards_screen.dart
+++ lib/presentation/cards/cards_screen.dart
@@ -36,6 +36,13 @@ class _CardsView extends StatelessWidget {
               _CardType1(elevation: card['elevation'], label: card['label'])),
           ...cards.map((card) =>
               _CardType2(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              _CardType3(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              _CardType4(elevation: card['elevation'], label: card['label'])),
+          const SizedBox(
+            height: 25,
+          )
         ],
       ),
     );
@@ -109,9 +116,92 @@ class _CardType2 extends StatelessWidget {
             Align(
               alignment: Alignment.bottomLeft,
               child: Text('$label - outline'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType3({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surface,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: Column(
+          children: [
+            Align(
+              //Alinear en la parte superior derecha
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType4({
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      //Evita q los hijos salgan del elemneto padre
+      clipBehavior: Clip.hardEdge,
+      color: colors.surface,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            //'https://i0.wp.com/www.lacasadeel.net/wp-content/uploads/2023/01/irondowney_xum2.jpg?resize=1068%2C601&ssl=1',
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            //Alinear en la parte superior derecha
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      topRight: Radius.circular(10))),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
