6acc148679860ec0ce9bf16999e05c81c2cdd16b
alejo Gonzalez <alejo.g31@hotmail.com>
2023-07-22 00:30:32 -0500

    Se agrega los snackbar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -25,5 +25,10 @@ final appRouter = GoRouter(
       name: ProgresScreen.name,
       builder: (context, state) => const ProgresScreen(),
     ),
+    GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
   ],
 );
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -3,10 +3,30 @@ import 'package:flutter/material.dart';
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
 
+  void showCustomSnackBar(BuildContext context) {
+    //Permite cerrar el primer snackbar antes de mostrar otro
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    ));
+  }
+
   const SnackbarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snacbars y Diálogo'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Stanckbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackBar(context),
+      ),
+    );
   }
 }
