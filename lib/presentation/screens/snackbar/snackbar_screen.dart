c9857c67ca89950baea34a55dffdd881d92d2d31
alejo Gonzalez <alejo.g31@hotmail.com>
2023-07-22 19:29:31 -0500

    Gestion de licencias y popup o dialogos

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
+
+  const SnackbarScreen({super.key});
 
   void showCustomSnackBar(BuildContext context) {
     //Permite cerrar el primer snackbar antes de mostrar otro
@@ -14,13 +17,57 @@ class SnackbarScreen extends StatelessWidget {
     ));
   }
 
-  const SnackbarScreen({super.key});
+  void openDialog(BuildContext context) {
+    //El bilder sigmifica q es algo q se construira en tiempo de ejecucion
+    showDialog(
+      context: context,
+      //Se obliga al usuario q seleccione una opcion
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estas seguro?'),
+        content: const Text(
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sit sapiente quas voluptas unde hic ex obcaecati, qui consequuntur necessitatibus quia fugiat repellat nemo quam, deserunt voluptatum perferendis. Ratione, vero.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snacbars y Diálogo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              onPressed: () {
+                showAboutDialog(context: context, children: [
+                  const Text(
+                      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae sit sapiente quas voluptas unde hic ex obcaecati, qui consequuntur necessitatibus quia fugiat repellat nemo quam, deserunt voluptatum perferendis. Ratione, vero.'),
+                ]);
+              },
+              child: const Text('Licencias Usadas'),
+            ),
+            FilledButton(
+              onPressed: () {
+                openDialog(context);
+              },
+              child: const Text('Mostrar dialogo'),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Stanckbar'),
